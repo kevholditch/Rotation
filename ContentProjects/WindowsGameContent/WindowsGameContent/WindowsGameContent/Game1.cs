2d8b0c404b21ff0af9825d8b9aaa45f1f3f0e825
Kevin Holditch <kevholditch@hotmail.com>
2013-01-07 20:55:49 +0000

    Added new project to build content for windows

--- ContentProjects/WindowsGameContent/WindowsGameContent/WindowsGameContent/Game1.cs
+++ ContentProjects/WindowsGameContent/WindowsGameContent/WindowsGameContent/Game1.cs
@@ -1,40 +1,28 @@
-﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
-using Rotation.Controls;
-using Rotation.Drawing.Animations;
-using Rotation.Engine;
-using Rotation.Events;
-using Rotation.StandardBoard;
-using Rotation.Textures;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
-
-namespace MonoRotation
+namespace WindowsGameContent
 {
     /// <summary>
     /// This is the main type for your game
     /// </summary>
-    public class RotationGame : Game, ITextureLoader
+    public class Game1 : Microsoft.Xna.Framework.Game
     {
-        private GraphicsDeviceManager graphics;
+        GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        private IBoard _board;
-        private IDrawEngine _drawEngine;
-        private readonly IContainer _container;
-        private IBoardFiller _boardFiller;
-        private readonly IAnimationEngine _animationEngine;
-        private IGameStateController _gameStateController;
 
-        public RotationGame(IContainer container, IBoard board, IBoardFiller boardFiller, IGameEventDispatcher gameEventDispatcher, IAnimationEngine animationEngine, IGameStateController gameStateController)
+        public Game1()
         {
-            _container = container;
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            _board = board;
-            _boardFiller = boardFiller;
-            _animationEngine = animationEngine;
-            _gameStateController = gameStateController;
-            GameEvents.Dispatcher = gameEventDispatcher;
         }
 
         /// <summary>
@@ -50,27 +38,17 @@ namespace MonoRotation
             base.Initialize();
         }
 
-
         /// <summary>
         /// LoadContent will be called once per game and is the place to load
         /// all of your content.
         /// </summary>
         protected override void LoadContent()
         {
-         
+            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            CurrentGraphicsDevice = GraphicsDevice;
 
-            _boardFiller.Fill(_board);
-            _drawEngine = _container.Resolve<IDrawEngine>();
-
-            _font = Content.Load<SpriteFont>("GameFont");
-            _gameStateController.Initialise();
-
+            // TODO: use this.Content to load your game content here
         }
-
-        private SpriteFont _font;
-        public static GraphicsDevice CurrentGraphicsDevice { get; private set; }
 
         /// <summary>
         /// UnloadContent will be called once per game and is the place to unload
@@ -88,10 +66,14 @@ namespace MonoRotation
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            _gameStateController.Update(gameTime);
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // TODO: Add your update logic here
+
             base.Update(gameTime);
         }
-
 
         /// <summary>
         /// This is called when the game should draw itself.
@@ -99,19 +81,11 @@ namespace MonoRotation
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.White);
+            GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend);
-            _animationEngine.Run(gameTime);
-            _drawEngine.Draw(spriteBatch);
-            spriteBatch.End();
+            // TODO: Add your drawing code here
 
             base.Draw(gameTime);
-        }
-
-        public Texture2D Load(string assetName)
-        {
-            return Content.Load<Texture2D>(assetName);
         }
     }
 }
